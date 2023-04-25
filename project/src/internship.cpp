d29751a6e94a0f514f36cdaf733735ae117bfc67
cellmer <michal.cellmer@gmail.com>
2023-04-25 18:27:18 +0200

    create vector containing all operating systems

--- project/src/internship.cpp
+++ project/src/internship.cpp
@@ -3,14 +3,16 @@
 #include <string>
 
 #include <date/date.h>
+#include <rapidjson/document.h>
+#include <rapidjson/istreamwrapper.h>
 
 #include "internship.h"
 
-
-namespace internship {
-
-    // releaseDate and eol must be dates in format YYYY-mm-dd
-    int calculateSupportPeriod(const std::string& releaseDate, const std::string& eol) {
+namespace internship
+{
+    // returns duration in days, releaseDate and eol must be dates in format YYYY-mm-dd
+    int calculateSupportPeriod(const std::string &releaseDate, const std::string &eol)
+    {
         // get objects describing both dates
         std::istringstream iss(releaseDate);
         date::sys_days releaseDateDays;
@@ -20,13 +22,48 @@ namespace internship {
         date::sys_days eolDays;
         iss >> date::parse("%F", eolDays);
 
-        // containing both starting and ending day
+        // contains both starting and ending day
         int duration_in_days = (eolDays - releaseDateDays).count() + 1;
 
         return duration_in_days;
     }
 
-    void solution(const std::string& jsonFileName, int elementsCount) {
-        std::cout << calculateSupportPeriod("2023-01-03", "2024-08-04") << "\n";
+    // populates vector with values about os from json file
+    void getOperatingSystemsFromJson(std::vector<OperatingSystem> &operatingSystems, const std::string &fileName)
+    {
+        std::ifstream ifs(fileName);
+        rapidjson::IStreamWrapper isw(ifs);
+        rapidjson::Document products;
+        products.ParseStream(isw);
+
+        for (const auto &product : products.GetArray())
+        {
+            if (product["os"].GetBool())
+            {
+                // reserve place for all versions to avoid copying in every iteration
+                operatingSystems.reserve(operatingSystems.size() + product["versions"].GetArray().Capacity());
+
+                // add all os versions to vector
+                for (const auto &os : product["versions"].GetArray())
+                {
+                    std::cout << "Product Name: " << product["name"].GetString() << "\n";
+
+                    operatingSystems.emplace_back(product["name"].GetString(),
+                                                  os["cycle"].GetString(),
+                                                  calculateSupportPeriod(os["releaseDate"].GetString(), os["eol"].GetString()));
+                }
+            }
+        }
+    }
+
+    void solution(const std::string &jsonFileName, int elementsCount)
+    {
+        std::vector<OperatingSystem> operatingSystems;
+        getOperatingSystemsFromJson(operatingSystems, jsonFileName);
+
+        for (const auto& os : operatingSystems)
+        {
+            std::cout << os.name << " " << os.cycle << " " << os.supportPeriod << "\n";
+        }
     }
 }
--- project/src/internship.h
+++ project/src/internship.h
@@ -1,7 +1,28 @@
 #pragma once
 #include <string>
+#include <vector>
 
-namespace internship {
-    // do not remove this function
-    void solution(const std::string& jsonFileName, int elementsCount);
+namespace internship
+{
+
+    struct OperatingSystem
+    {
+        std::string name;
+        std::string cycle;
+        int supportPeriod;
+
+        OperatingSystem(std::string name, std::string cycle, int supportPeriod) : 
+            name(name), cycle(cycle), supportPeriod(supportPeriod)
+        {
+        }
+    };
+
+    // returns duration in days, releaseDate and eol must be dates in format YYYY-mm-dd
+    int calculateSupportPeriod(const std::string &releaseDate, const std::string &eol);
+
+    // populates vector with values about os from json file
+    void getOperatingSystemsFromJson(std::vector<OperatingSystem> &operatingSystems, const std::string &fileName);
+
+    void solution(const std::string &jsonFileName, int elementsCount);
+
 }
